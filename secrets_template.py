951118937a3da08978dfb9219b4d6e35a9c3d750
Alejandro Sanchez <a-sanchez@ispace-inc.com>
2025-08-01 16:38:59 +0900

    Add secrets template (WIP)

---
+++ secrets_template.py
@@ -0,0 +1,10 @@
+# secrets.py
+
+SPOTIPY_CLIENT_ID     = ""
+SPOTIPY_CLIENT_SECRET = ""
+SPOTIPY_REDIRECT_URL  = "https://example.com/callback"
+SPOTIPY_REFRESH_TOKEN = ""
+
+EMAIL_SENDER   = "" # Email address used to send the emails
+EMAIL_PASSWORD = "" # App-specific or regular if no 2FA
+EMAIL_RECEIVER = "" # Email address used to receive the emails
